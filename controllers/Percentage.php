d7b533600560804170a0e1158edacc6c4a1c3c62
Flavio Rosselli <rosselli00@gmail.com>
2019-03-12 11:42:18 -0500

    Percentage was refactored.

--- controllers/Percentage.php
+++ controllers/Percentage.php
@@ -1,44 +1,16 @@
 <?php
 namespace Controllers;
 
-class Percentage implements CalculatorPercentage {
-	public function calculate(float $a, float $b): float {
-		return 100 - (($b * 100)/$a);
-	}
-
-	public function whatIsPPercentOfX(float $p, float $x): float {
-		return $p * $x;
-	}
-
-	public function yIsWhatPercentOfX(float $y, float $x): float {
-		return ($y/$x) * 100;
-	}
-
-	public function yIsPPercentOfWhat(float $y, float $p): float {
-		return $y/($p/100);
-	}
-
-	public function whatPercentOfXIsY(float $x, float $y): float {
-		return ($y/$x)*100;
-	}
-
-	public function pPercentOfWhatIsY(float $y, float $p): float {
-		return $y/($p/100);
-	}
-
-	public function PPercentOfXIsWhat(float $p, float $x): float {
+class Percentage {
+	public function findPercent(float $p, float $x): float {
 		return ($p/100)*$x;
 	}
 
-	public function YOfWhatIsPPercent(float $y, float $p): float {
-		return $y/($p/100);
-	}
-
-	public function whatOfXIsPPercent(float $p, float $x): float {
-		return ($p/100)*$x;
-	}
-
-	public function yOfXIsWhatPercent(float $y, float $x): float {
+	public function findPercentage(float $y, float $x): float {
 		return ($y/$x)*100;
+	}
+
+	public function findTotal(float $y, float $p): float {
+		return $y/($p/100);
 	}
 }
--- tests/PercentageTest.php
+++ tests/PercentageTest.php
@@ -10,43 +10,25 @@ class PercentageTest extends TestCase {
 		$this->p = new Percentage();
 	}
 
-	public function testPercentage() {
-		$this->assertEquals(25, number_format($this->p->calculate(0.8, 0.6), 2));
+//	public function testPercentage() {
+//		$this->assertEquals(25, number_format($this->p->calculate(0.8, 0.6), 2));
+//	}
+//
+	public function testFindPercent() {
+		$this->assertEquals(1.45, number_format($this->p->findPercent(5, 29), 2));
+		$this->assertEquals(2.5, number_format($this->p->findPercent(10, 25), 2));
+		$this->assertEquals(2.75, number_format($this->p->findPercent(11, 25), 2));
 	}
 
-	public function testWhatIsPPercentOfX() {
-		$this->assertEquals(2.5, number_format($this->p->whatIsPPercentOfX(0.1, 25), 2));
+	public function testFindPercentage() {
+		$this->assertEquals(30, number_format($this->p->findPercentage(12, 40), 2));
+		$this->assertEquals(69.23, number_format($this->p->findPercentage(9, 13), 2));
+		$this->assertEquals(22.22, number_format($this->p->findPercentage(6, 27), 2));
 	}
 
-	public function testYIsWhatPercentOfX() {
-		$this->assertEquals(30, number_format($this->p->yIsWhatPercentOfX(12, 40), 2));
-	}
-
-	public function testYIsPPercentOfWhat() {
-		$this->assertEquals(15, number_format($this->p->yIsPPercentOfWhat(9, 60), 2));
-	}
-
-	public function testWhatPercentOfXIsY() {
-		$this->assertEquals(22.22, number_format($this->p->whatPercentOfXIsY(27, 6), 2));
-	}
-
-	public function testPPercentOfWhatIsY() {
-		$this->assertEquals(35, number_format($this->p->PPercentOfWhatIsY(7, 20), 2));
-	}
-
-	public function testPPercentOfXIsWhat() {
-		$this->assertEquals(1.45, number_format($this->p->pPercentOfXIsWhat(5, 29), 2));
-	}
-
-	public function testYOfWhatIsPPercent() {
-		$this->assertEquals(33.33, number_format($this->p->yOfWhatIsPPercent(4, 12), 2));
-	}
-
-	public function testWhatOfXIsPPercent() {
-		$this->assertEquals(2.75, number_format($this->p->whatOfXIsPPercent(11, 25), 2));
-	}
-
-	public function testYOfXIsWhatPercent() {
-		$this->assertEquals(69.23, number_format($this->p->yOfXIsWhatPercent(9, 13), 2));
+	public function testFindTotal() {
+		$this->assertEquals(15, number_format($this->p->findTotal(9, 60), 2));
+		$this->assertEquals(33.33, number_format($this->p->findTotal(4, 12), 2));
+		$this->assertEquals(35, number_format($this->p->findTotal(7, 20), 2));
 	}
 }
